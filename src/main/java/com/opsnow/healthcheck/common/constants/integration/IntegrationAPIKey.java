965b2b95460bf912d87a69a8abbd317c8d1012bd
이지성Jiseong Lee <jiseong.lee@bespinglobal.com>
2021-04-07 10:54:07 +0900

    [AN1-879] 테스트 및 기능 문서화 - 테스트용

--- src/main/java/com/opsnow/healthcheck/common/constants/integration/IntegrationAPIKey.java
+++ src/main/java/com/opsnow/healthcheck/common/constants/integration/IntegrationAPIKey.java
@@ -2,8 +2,8 @@ package com.opsnow.healthcheck.common.constants.integration;
 
 
 public enum IntegrationAPIKey {
-    DEV_STANDARD_URL(EnvType.DEV, IntegrationType.standard, "6672be9c48620311eb1b86430a960e68e6c8"),
-    DEV_STANDARD_URL2(EnvType.DEV, IntegrationType.standard, "830c7faf292a3511eb7b86420a960e68e6c8");
+    DEV_STANDARD_URL(EnvType.DEV, IntegrationType.standard, "6672be9c48620311eb1b86430a960e68e6c8");
+//    DEV_STANDARD_URL2(EnvType.DEV, IntegrationType.standard, "830c7faf292a3511eb7b86420a960e68e6c8");
 
     private final String apiKey;
     private final EnvType envType;
